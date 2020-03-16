a1d0c3ed7552630d22c0e32d49555679b8bf1a7a
jeesoo <jeesoo.hong@gmail.com>
2020-03-17 00:11:08 +0900

    rate limiting 설정, prettier 설정

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,26 @@
-import express from "express";
-import api from "./controller";
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+
+import api from './controller';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send(`JUICE SERVER WORKS(env: ${process.env.NODE_ENV})`))
-app.get('/health', api.status)
-app.get('/issues', api.issues.get)
-app.get('/issues/:id', api.issues.getIssuesById)
-app.get('/pledges', api.pledges.get)
-app.get('/pledges/:id', api.pledges.getPledgeById)
-app.get('/result', api.result.get)
-app.post('/result', api.result.add)
-app.post('/personal', api.personal.add)
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
+app.get('/', (req, res) => res.send(`JUICE SERVER WORKS(env: ${process.env.NODE_ENV})`));
+app.get('/health', api.status);
+app.get('/issues', api.issues.get);
+app.get('/pledges', api.pledges.get);
+app.get('/pledges/:id', api.pledges.getPledgeById);
+app.get('/result', api.result.get);
+app.post('/result', api.result.add);
+app.get('/personal', api.personal.get);
+app.post('/personal', api.personal.add);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
