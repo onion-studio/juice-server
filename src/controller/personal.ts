7095b949b0379a0c06ff6e20f10ff948485db4d9
jeesoo <jeesoo.hong@gmail.com>
2020-03-18 00:33:10 +0900

    포맷 적용, 추가 디펜던시 설치

--- src/controller/personal.ts
+++ src/controller/personal.ts
@@ -1,14 +1,14 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
 const get = async (req: Request, res: Response): Promise<void> => {
-    // 추가 필요
+  // 추가 필요
 };
 
 const add = async (req: Request, res: Response): Promise<void> => {
-    // 추가 필요
+  // 추가 필요
 };
 
 export default {
-    get,
-    add
-}
+  get,
+  add,
+};
