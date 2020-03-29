162b2538ee998cbc8f0752279de037c10493988d
jeesoo <jeesoo.hong@gmail.com>
2020-03-30 00:28:04 +0900

    greenlock 설정, result route 변경

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import rateLimit from 'express-rate-limit';
 import cors from 'cors';
 import api from './controller';
+import path from 'path';
 
 const app = express();
 const port = 3000;
@@ -16,8 +17,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
 app.use(limiter);
-
 app.get('/', (req, res) => res.send(`JUICE SERVER WORKS(env: ${process.env.NODE_ENV})`));
 app.get('/health', api.status);
 app.post('/init', api.init.add);
@@ -26,7 +27,25 @@ app.get('/pledges', api.pledges.get);
 app.get('/pledges/:id', api.pledges.getPledgeById);
 app.get('/result', api.result.get);
 app.post('/result', api.result.add);
-app.get('/personal', api.personal.get);
+app.get('/result/pledges', api.result.getPledgesTotalCount);
+app.get('/result/juice', api.result.getJuice);
 app.post('/personal', api.personal.add);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+if (process.env.NODE_ENV === 'production') {
+  require('greenlock-express')
+    .init({
+      packageRoot: path.resolve(__dirname, '..'),
+      configDir: './greenlock.d',
+
+      // contact for security and critical bug notices
+      maintainerEmail: 'onionstudio.kr@gmail.com',
+
+      // whether or not to run at cloudscale
+      cluster: false,
+    })
+    // Serves on 80 and 443
+    // Get's SSL certificates magically!
+    .serve(app);
+} else {
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+}
