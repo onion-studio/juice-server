58b59637dad19694debaa86f3169249a3dec1884
jeesoo <jeesoo.hong@gmail.com>
2020-03-30 13:40:09 +0900

    typo fixed

--- src/model/init.ts
+++ src/model/init.ts
@@ -2,7 +2,7 @@ import { poolQuery } from '../pool';
 
 const add = async (token: string, timestamp: string): Promise<void> => {
   const q = `
-    INSERT INTO user (uid, created_at)
+    INSERT INTO user (uuid, created_at)
     VALUES ( ?, ? )
   `;
   const args = [token, timestamp];
