5578c984e7cfddb542b8fc0a081879b517d96e3a
Kim Seungha <seungha.me@gmail.com>
2021-05-13 00:05:48 +0900

    default value for browser, ip_address
    원래는 문제없이 동작하던 코드인데, 동작하지 않음
    DB 제약사항이 바뀌었거나, 라이브러리가 조용히 처리해주던 것이 업데이트 되었나봄

--- src/model/init.ts
+++ src/model/init.ts
@@ -2,8 +2,8 @@ import { poolQuery } from '../pool';
 
 const add = async (token: string, timestamp: string): Promise<void> => {
   const q = `
-    INSERT INTO users (uuid, created_at)
-    VALUES ( ?, ? )
+    INSERT INTO users (uuid, created_at, browser, ip_address)
+    VALUES ( ?, ?, '', '' )
   `;
   const args = [token, timestamp];
   return await poolQuery(q, args);
